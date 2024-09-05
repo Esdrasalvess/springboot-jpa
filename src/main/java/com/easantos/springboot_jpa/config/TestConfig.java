a7749aeeb0a77d64f0dbf8a346542e0c6f782c40
Esdras Alves <esdrasalvesdossantos2002@gmail.com>
2024-09-05 11:40:37 -0300

    OrderStatus enum

--- src/main/java/com/easantos/springboot_jpa/config/TestConfig.java
+++ src/main/java/com/easantos/springboot_jpa/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.data.repository.ListCrudRepository;
 
 import com.easantos.springboot_jpa.entities.Order;
 import com.easantos.springboot_jpa.entities.User;
+import com.easantos.springboot_jpa.entities.enums.OrderStatus;
 import com.easantos.springboot_jpa.repositories.OrderRepository;
 import com.easantos.springboot_jpa.repositories.UserRepository;
 
@@ -32,9 +33,9 @@ public class TestConfig implements CommandLineRunner{
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2024-08-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2024-08-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2024-08-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2024-08-20T19:53:07Z"),OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2024-08-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2024-08-22T15:21:22Z"),OrderStatus.WAITING_PAYMENT, u1);
 		
 		((ListCrudRepository<User, Long>) userRepository).saveAll(Arrays.asList(u1,u2));
 		((ListCrudRepository<Order, Long>) orderRepository).saveAll(Arrays.asList(o1,o2, o3));
